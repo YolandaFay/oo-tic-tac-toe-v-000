205d7accbfe27d92655f934be3f49b26d4323f94
Yolanda Fay <yolandafay@gmail.com>
2018-08-04 02:52:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,8 +31,7 @@ class TicTacToe
      index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn_count(board)
-
+  def turn_count
     counter = 0 
     @board.each do |entry|
       if entry != " "
